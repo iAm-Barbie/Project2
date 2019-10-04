605a92a2fe15328a842bd8c569c6b03e3d28b2f9
danyel oliveira <oliveiradanyel901@gmail.com>
2019-10-03 19:48:06 -0600

    login form

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,42 @@
 var db = require("../models");
+//------------------------------
+var path = require("path");
+
+var isAuthenticated = require("../config/middleware/isAutenticated");
 
 module.exports = function(app) {
-  // Load index page
+
   app.get("/", function(req, res) {
+    // If the user already has an account send them to the members page
+    // if (req.user) {
+    //   res.redirect("/members");
+    // }
+    // res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("signup");
+  });
+
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/home");
+    }
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render('login');
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, function(req, res) {
+    // res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.render('members');
+  });
+
+//};
+//--------------------------------------------
+
+//module.exports = function(app) {
+  // Load index page
+  app.get("/home", isAuthenticated, function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
