6a56a170a7aeff30f6426e5b7f27e819c4c43e4f
BarbieDoLLxo <barbie.slumerican@gmail.com>
2019-10-07 13:27:52 -0600

    final style

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,9 @@ CREATE DATABASE exampledb;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+
+DROP DATABASE IF EXISTS nodejs_login;
+CREATE DATABASE nodejs_login;
+USE  nodejs_login;
+
+ALTER TABLE examples ADD times varchar (30) after description;
